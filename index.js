9ce4a8cf466376b2ce32e164c02a018423fd91cf
giulioe-star <giulio_e@tiscali.it>
2026-04-22 13:45:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -43,4 +43,6 @@ app.get("/", async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running on port " + PORT));
+
